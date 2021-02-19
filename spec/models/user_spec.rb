9cab67251883437090911457c88e67ac8214cc23
Stephan Ellenberger <s.ellenberger95@gmail.com>
2021-02-19 16:17:31 +0000

    Validates presence of first and last name and supporting specs in user class

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,58 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "can make a new instance of a user" do
+    user = User.create(
+      email: 'example@example.com', 
+      password: 'password', 
+      password_confirmation: 'password', 
+      first_name: 'John', 
+      last_name: 'Smith'
+    )
+    expect(user).to be_valid
+  end
+
+  it 'will not be valid without an email address' do
+    user = User.create(
+      email: '', 
+      password: 'password', 
+      password_confirmation: 'password', 
+      first_name: 'John', 
+      last_name: 'Smith'
+    )
+    expect(user).to_not be_valid
+  end
+
+  it 'will not be valid if passwords dont match' do
+    user = User.create(
+      email: 'example@example.com', 
+      password: 'password', 
+      password_confirmation: 'passwor', 
+      first_name: 'John', 
+      last_name: 'Smith'
+    )
+    expect(user).to_not be_valid
+  end
+
+  it 'first name has to be at least 2 characters long' do
+    user = User.create(
+      email: 'example@example.com', 
+      password: 'password', 
+      password_confirmation: 'password', 
+      first_name: 'J', 
+      last_name: 'Smith'
+    )
+    expect(user).to_not be_valid
+  end
+
+  it 'last name has to be at least 2 characters long' do
+    user = User.create(
+      email: 'example@example.com', 
+      password: 'password', 
+      password_confirmation: 'password', 
+      first_name: 'John', 
+      last_name: 'S'
+    )
+    expect(user).to_not be_valid
+  end
 end
